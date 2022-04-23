6f5512c2fc1183200c1e0d65d0abbc8e323f6164
A <98947793+AnA222444@users.noreply.github.com>
2022-04-23 23:41:37 +0530

    Update AllTests.java

--- src/JunitTest_lab10/AllTests.java
+++ src/JunitTest_lab10/AllTests.java
@@ -7,6 +7,5 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ testAddNumbers.class, testAddStrings.class })
 
-public class AllTests {
-
+public class AllTests{
 }
